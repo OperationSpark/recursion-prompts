f2129abb6457bd5c7aa10af9b7d0693d1fab13b7
Alex Aaron <alexfrancisaaron@gmail.com>
2025-06-04 12:09:05 -0500

    updated spec/part2.js to arrow functions and let/const'

--- spec/part2.js
+++ spec/part2.js
@@ -1,23 +1,23 @@
 (function () {
   'use strict';
 
-  describe('More Exercises in Recursion', function () {
-    describe('36. Count tags', function () {
-      var htmlStrings = [
+  describe('More Exercises in Recursion', () => {
+    describe('36. Count tags', () => {
+      const htmlStrings = [
         '<p class="recursionTest"></p>',
         '<p class="otherClassName recursionTest"></p>',
         '<p><div class="somediv"><div class="innerdiv"><span class="recursionTest">yay</span></div></div></p>',
       ];
 
-      var result;
-      var expectedTagCount;
-      var originalTagCount = tagCount;
+      let result;
+      let expectedTagCount;
+      let originalTagCount = tagCount;
       tagCount = sinon.spy(tagCount);
 
-      it('should return number of times of tag occurs on node', function () {
+      it('should return number of times of tag occurs on node', () => {
         $('body').addClass('recursionTest');
         htmlStrings.forEach(function (htmlString) {
-          var $rootElement = $(htmlString);
+          const $rootElement = $(htmlString);
           $('body').append($rootElement);
 
           result = tagCount('p');
@@ -29,34 +29,34 @@
         $('body').removeClass('recursionTest');
       });
 
-      it('should return a number', function () {
+      it('should return a number', () => {
         expect(typeof result).to.equal('number');
       });
 
-      it('should use recursion by calling self', function () {
+      it('should use recursion by calling self', () => {
         expect(tagCount.callCount).to.be.above(1);
         tagCount = originalTagCount;
       });
     });
 
-    describe('37. Binary Search', function () {
-      var input1 = [1, 2, 3, 4, 5, 6];
-      var input2 = [1, 2, 3, 4, 5, 6, 7];
-      var primes = [
+    describe('37. Binary Search', () => {
+      const input1 = [1, 2, 3, 4, 5, 6];
+      const input2 = [1, 2, 3, 4, 5, 6, 7];
+      const primes = [
         2, 3, 5, 7, 11, 13, 17, 19, 23, 29, 31, 37, 41, 43, 47, 53, 59, 61, 67,
         71, 73, 79, 83, 89, 97,
       ];
 
-      it('should return a number', function () {
+      it('should return a number', () => {
         expect(typeof binarySearch(input1, 3)).to.eql('number');
       });
 
-      it('should not mutate the input array', function () {
-        var result = binarySearch(input1, 4);
+      it('should not mutate the input array', () => {
+        const result = binarySearch(input1, 4);
         expect(input1).to.eql([1, 2, 3, 4, 5, 6]);
       });
 
-      it('should return null if target not found', function () {
+      it('should return null if target not found', () => {
         expect(binarySearch(input1, 7)).to.eql(null);
         expect(binarySearch(input2, 8)).to.eql(null);
         expect(binarySearch(primes, 32)).to.eql(null);
@@ -83,8 +83,8 @@
         expect(binarySearch(primes, 97)).to.eql(24);
       });
 
-      it('should use recursion by calling self', function () {
-        var originalBinarySearch = binarySearch;
+      it('should use recursion by calling self', () => {
+        const originalBinarySearch = binarySearch;
         binarySearch = sinon.spy(binarySearch);
         binarySearch(primes, 19);
         expect(binarySearch.callCount).to.be.above(1);
@@ -92,32 +92,32 @@
       });
     });
 
-    describe('38. Merge Sort', function () {
-      var numbers = [8, 2, 20, 1, 15];
+    describe('38. Merge Sort', () => {
+      const numbers = [8, 2, 20, 1, 15];
 
-      it('should return an array', function () {
-        var sortedNumbers = mergeSort(numbers);
+      it('should return an array', () => {
+        const sortedNumbers = mergeSort(numbers);
         expect(Array.isArray(sortedNumbers)).to.equal(true);
       });
 
-      it('should not mutate the input array', function () {
-        var sortedNumbers = mergeSort(numbers);
+      it('should not mutate the input array', () => {
+        const sortedNumbers = mergeSort(numbers);
         expect(numbers).to.eql([8, 2, 20, 1, 15]);
       });
 
-      it('should sort an array of numbers in order of least to greatest', function () {
-        var sortedNumbers = mergeSort(numbers);
+      it('should sort an array of numbers in order of least to greatest', () => {
+        const sortedNumbers = mergeSort(numbers);
         expect(sortedNumbers).to.eql([1, 2, 8, 15, 20]);
       });
 
-      it('should be able to handle negative numbers', function () {
-        var numbers = [8, -2, 20, 1, -15];
-        var sortedNumbers = mergeSort(numbers);
+      it('should be able to handle negative numbers', () => {
+        const numbers = [8, -2, 20, 1, -15];
+        const sortedNumbers = mergeSort(numbers);
         expect(sortedNumbers).to.eql([-15, -2, 1, 8, 20]);
       });
 
-      it('should use recursion by calling self', function () {
-        var originalMergeSort = mergeSort;
+      it('should use recursion by calling self', () => {
+        const originalMergeSort = mergeSort;
         mergeSort = sinon.spy(mergeSort);
         mergeSort(numbers);
         expect(mergeSort.callCount).to.be.above(1);
